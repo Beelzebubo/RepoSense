e60deef7dc9ad7018ad4008d436b8fb5b2cc5c38
Saumya Bikram Bartaula <bartaulasaumya@gmail.com>
2026-08-30 14:25:20 +0545

    fixed some stuff in the initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defaultConfig } from 'vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig ({
-    plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            '/proxy/codeload': {
-                target: 'https://codeload.github.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/proxy\/codeload/, ''),
-            },
-        },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/proxy/codeload': {
+        target: 'https://codeload.github.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy\/codeload/, ''),
+      },
     },
+  },
 })
